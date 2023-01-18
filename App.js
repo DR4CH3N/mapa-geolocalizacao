d387e2016727f6d311c38b1babeea50af10d0af5
Matheus Anthony <thehellc4t@gmail.com>
2023-01-18 11:11:29 -0300

    configuração do marker

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StyleSheet, StatusBar, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
   const regiaoInicial = {
@@ -7,6 +7,12 @@ export default function App() {
     longitude: -123.1207,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
+  };
+  const localizacao = {
+    latitude: 37.563936,
+    longitude: -116.85123,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
   };
   return (
     <>
@@ -16,11 +22,20 @@ export default function App() {
           style={estilos.mapa}
           initialRegion={regiaoInicial}
           liteMode={false} // apenas android
-          mapType="standard" // satellite, hybrid, standard
+          mapType="satellite" // opções: satellite, hybrid, standard
           userInterfaceStyle="dark" // somente IOS
           maxZoomLevel={15}
           minZoomLevel={4}
-        />
+        >
+          <Marker
+            draggable
+            title="Mira aqui!"
+            coordinate={localizacao}
+            onPress={(event) => {
+              console.log(event.nativeEvent);
+            }}
+          />
+        </MapView>
       </View>
     </>
   );
